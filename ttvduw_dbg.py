d2181a0698a27a204f7f034b2f66f4d256d0840a
y0umu <antitankxzc@gmail.com>
2022-02-19 16:47:04 +0800

    added some memory debugging code

---
+++ ttvduw_dbg.py
@@ -0,0 +1,75 @@
+'''
+Debug helpers
+'''
+# total_size() is copied from https://code.activestate.com/recipes/577504/
+from __future__ import print_function
+from sys import getsizeof, stderr
+from itertools import chain
+from collections import deque
+try:
+    from reprlib import repr
+except ImportError:
+    pass
+
+def total_size(o, handlers={}, verbose=False):
+    """ Returns the approximate memory footprint an object and all of its contents.
+
+    Automatically finds the contents of the following builtin containers and
+    their subclasses:  tuple, list, deque, dict, set and frozenset.
+    To search other containers, add handlers to iterate over their contents:
+
+        handlers = {SomeContainerClass: iter,
+                    OtherContainerClass: OtherContainerClass.get_elements}
+
+    """
+    dict_handler = lambda d: chain.from_iterable(d.items())
+    all_handlers = {tuple: iter,
+                    list: iter,
+                    deque: iter,
+                    dict: dict_handler,
+                    set: iter,
+                    frozenset: iter,
+                   }
+    all_handlers.update(handlers)     # user handlers take precedence
+    seen = set()                      # track which object id's have already been seen
+    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+
+    def sizeof(o):
+        if id(o) in seen:       # do not double count the same object
+            return 0
+        seen.add(id(o))
+        s = getsizeof(o, default_size)
+
+        if verbose:
+            print(s, type(o), repr(o), file=stderr)
+
+        for typ, handler in all_handlers.items():
+            if isinstance(o, typ):
+                s += sum(map(sizeof, handler(o)))
+                break
+        return s
+
+    return sizeof(o)
+
+
+# def mem_usage_of(x):
+#     '''
+#     print size of variables in x
+#     x can be a list-like or dict
+#     '''
+#     print('-------------------------------')
+#     print(f"name\t\tbytes")
+#     if type(x) is dict:
+#         for k, v in x.items():
+#             print(f'{k}\t\t{getsizeof(v)}')
+#     elif type(x) is list or tuple or set:
+#         for v in x:
+#             print(f'{v}\t\t{getsizeof(v)}')
+#     print('-------------------------------\n')
+
+
+##### Example call #####
+
+if __name__ == '__main__':
+    d = dict(a=1, b=2, c=3, d=[4,5,6,7], e='a string of chars')
+    print(total_size(d, verbose=True))
